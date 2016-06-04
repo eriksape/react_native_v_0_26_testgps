b903ceac9ee1fc35ac34756361422bef6f14e357
Erik R Sánchez Pérez <erik.sape@gmail.com>
2016-06-04 11:47:18 -0500

    prueba gas

--- index.ios.js
+++ index.ios.js
@@ -12,41 +12,51 @@ import {
   View
 } from 'react-native';
 
-class testgps extends Component {
+const testgps = React.createClass({
+  getInitialState() {
+    return {
+      initialPosition: 'unknown',
+      lastPosition: 'unknown',
+    }
+  },
+  componentDidMount: function() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        var initialPosition = JSON.stringify(position);
+        this.setState({initialPosition});
+      },
+      (error) => alert(error.message),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+    );
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      var lastPosition = JSON.stringify(position);
+      this.setState({lastPosition});
+    });
+  },
+
+  componentWillUnmount: function() {
+    navigator.geolocation.clearWatch(this.watchID);
+  },
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+      <View>
+        <Text>Hola amigo</Text>
+        <Text>
+          <Text style={styles.title}>Initial position: </Text>
+          {this.state.initialPosition}
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+        <Text>
+          <Text style={styles.title}>Current position: </Text>
+          {this.state.lastPosition}
         </Text>
       </View>
     );
   }
-}
+})
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  title: {
+    fontWeight: '500',
   },
 });
 
